d805527eba88575f7fdcb27b3a5686cac2619318
jayjuk <me@jayjoseph.com>
2024-03-13 10:26:32 +0000

    Script to run unit tests like GHA does

---
+++ run_unit_tests.bat
@@ -0,0 +1,10 @@
+@echo off
+tasklist /FI "IMAGENAME eq Docker Desktop.exe" 2>NUL | find /I /N "Docker Desktop.exe">NUL
+if "%ERRORLEVEL%"=="1" (
+    echo Launching Docker Desktop...
+    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    echo Waiting for Docker Desktop to start...
+    timeout /T 10
+)
+echo Running unit tests as per GHA using Act
+"C:\Act\Act.exe" -j test
