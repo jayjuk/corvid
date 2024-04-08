5767ae20e00ff9398db767236ce1b226fdf3a165
jayjuk <me@jayjoseph.com>
2024-04-08 18:37:15 +0100

    type hints

--- common/azurestoragemanager.py
+++ common/azurestoragemanager.py
@@ -2,9 +2,10 @@ from storagemanager import StorageManager
 from shutil import copy2
 from logger import setup_logger, exit, debug
 from azure.core.credentials import AzureNamedKeyCredential
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerClient
 from azure.data.tables import TableServiceClient, UpdateMode
 from utils import get_critical_env_variable
+from typing import Optional, Dict, List, Any
 
 # Set up logger
 logger = setup_logger()
@@ -16,24 +17,26 @@ import logging
 class AzureStorageManager(StorageManager):
 
     # Constructor
-    def __init__(self, image_only=False):
+    def __init__(self, image_only: bool = False) -> None:
+        # Call parent constructor
         super().__init__(image_only)
-        self.sas_token_expiry_time = None
-        self.sas_token = None
-
         # Get and remember credential
-        self.credential = self.get_azure_credential()
+        self.credential: Optional[AzureNamedKeyCredential] = self.get_azure_credential()
         if self.credential:
             # Get Azure storage client
             if image_only:
-                self.table_service_client = None
+                self.table_service_client: Optional[TableServiceClient] = None
                 logger.info("Running in images only mode - no table storage")
             else:
-                self.table_service_client = self.get_azure_table_service_client()
+                self.table_service_client: Optional[TableServiceClient] = (
+                    self.get_azure_table_service_client()
+                )
 
             # Get Azure storage client
-            self.blob_service_client = self.get_azure_blob_service_client()
-            self.image_container_name = "jaysgameimages"
+            self.blob_service_client: Optional[BlobServiceClient] = (
+                self.get_azure_blob_service_client()
+            )
+            self.image_container_name: str = "jaysgameimages"
         else:
             exit(logger, "Could not get Azure credential.")
 
@@ -47,18 +50,11 @@ class AzureStorageManager(StorageManager):
         logging.getLogger("azure").setLevel(logging.WARNING)
 
         # Cache of data types to convert to/from JSON to strings when storing
-        self.complex_variable_cache = {}
-
-    # Return Azure credential
-    def get_azure_credential(self):
-        return AzureNamedKeyCredential(
-            get_critical_env_variable("AZURE_STORAGE_ACCOUNT_NAME"),
-            get_critical_env_variable("AZURE_STORAGE_ACCOUNT_KEY"),
-        )
+        self.complex_variable_cache: dict = {}
 
     # Return Azure table service client
     # Assumes if we have a credential, we have AZURE_STORAGE_ACCOUNT_NAME set
-    def get_azure_table_service_client(self):
+    def get_azure_table_service_client(self) -> TableServiceClient:
 
         return TableServiceClient(
             endpoint="https://"
@@ -67,7 +63,7 @@ class AzureStorageManager(StorageManager):
             credential=self.credential,
         )
 
-    def get_azure_blob_service_client(self):
+    def get_azure_blob_service_client(self) -> BlobServiceClient:
         return BlobServiceClient(
             account_url="https://"
             + get_critical_env_variable("AZURE_STORAGE_ACCOUNT_NAME")
@@ -75,19 +71,23 @@ class AzureStorageManager(StorageManager):
             credential=self.credential,
         )
 
-    def check_rowkey(self, table_client, query_filter_rowkey):
-        parameters = {"pk": "jaysgame", "rk": query_filter_rowkey}
-        query_filter = "PartitionKey eq @pk and RowKey eq @rk"
+    def check_rowkey(
+        self, table_client: TableServiceClient, query_filter_rowkey: str
+    ) -> bool:
+        parameters: dict = {"pk": "jaysgame", "rk": query_filter_rowkey}
+        query_filter: str = "PartitionKey eq @pk and RowKey eq @rk"
         for _ in table_client.query_entities(query_filter, parameters=parameters):
             return True
         return False
 
-    def store_image_in_cloud(self, world_name, image_name, image_data):
+    def store_image_in_cloud(
+        self, world_name: str, image_name: str, image_data: bytes
+    ) -> bool:
         logger.info(f"Uploading image '{image_name}' to cloud")
 
         # Get a reference to the container
-        container_client = self.blob_service_client.get_container_client(
-            self.image_container_name
+        container_client: Optional[ContainerClient] = (
+            self.blob_service_client.get_container_client(self.image_container_name)
         )
 
         # Create the container if it doesn't exist
@@ -114,20 +114,20 @@ class AzureStorageManager(StorageManager):
             logger.error(f"Error uploading image: {e}")
         return False
 
-    def get_image_url(self, world_name, image_name):
+    def get_image_url(self, world_name: str, image_name: str) -> Optional[str]:
         if image_name:
             logger.info(
                 f"Resolving image URL for world {world_name} / image {image_name}"
             )
-            hostname = get_critical_env_variable("IMAGESERVER_HOSTNAME")
-            port = get_critical_env_variable("IMAGESERVER_PORT")
+            hostname: str = get_critical_env_variable("IMAGESERVER_HOSTNAME")
+            port: str = get_critical_env_variable("IMAGESERVER_PORT")
             return f"http://{hostname}:{port}/image/{self.get_blob_name(world_name, image_name)}"
         return None
 
-    def get_image_blob(self, blob_name):
+    def get_image_blob(self, blob_name: str) -> Optional[bytes]:
         if self.image_container_name:
             logger.info(f"Downloading {blob_name} from Azure blob storage")
-            blob_client = self.blob_service_client.get_blob_client(
+            blob_client: BlobServiceClient = self.blob_service_client.get_blob_client(
                 self.image_container_name, blob_name
             )
             if blob_client:
@@ -137,7 +137,7 @@ class AzureStorageManager(StorageManager):
         return None
 
     # Store all Python objects, received as actual objects
-    def store_game_object(self, game_name, object):
+    def store_game_object(self, game_name: str, object: object) -> bool:
         logger.debug(f"Storing python object in game {game_name}: {object.__dict__}")
 
         objects_client = self.table_service_client.create_table_if_not_exists(
@@ -146,7 +146,7 @@ class AzureStorageManager(StorageManager):
 
         # Store object in Azure
         # Convert to dict
-        entity = object.__dict__.copy()
+        entity: dict = object.__dict__.copy()
         entity["PartitionKey"] = game_name + "__" + type(object).__name__
 
         # Don't store objects named "system" - they are transient
@@ -181,12 +181,14 @@ class AzureStorageManager(StorageManager):
         return True
 
     # Returns all instances of a type of object, as a dict
-    def get_game_objects(self, world_name, object_type, rowkey_value=None):
+    def get_game_objects(
+        self, world_name: str, object_type: str, rowkey_value: Optional[str] = None
+    ) -> List[Dict[str, Any]]:
         objects_client = self.table_service_client.get_table_client("PythonObjects")
         if objects_client:
-            objects = []
-            parameters = {"pk": world_name + "__" + object_type}
-            query_filter = "PartitionKey eq @pk"
+            objects: List[Dict[str, Any]] = []
+            parameters: Dict[str, str] = {"pk": world_name + "__" + object_type}
+            query_filter: str = "PartitionKey eq @pk"
             if rowkey_value:
                 parameters["rk"] = rowkey_value
                 query_filter += " and RowKey eq @rk"
