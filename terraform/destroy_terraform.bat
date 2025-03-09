d585cad87e2acc795f1eb703504e28be0f228b2e
jayjuk <me@jayjoseph.com>
2025-03-09 11:05:29 +0000

    fix destroy terraform glitch

--- terraform/destroy_terraform.bat
+++ terraform/destroy_terraform.bat
@@ -1,6 +1,23 @@
 @echo off
-REM Set some variables
-set "TF_VAR_AI_COUNT=1"
+REM Check for .env file in ..\common
+if exist "..\common\.env" (
+    set "env_file_path=..\common\.env"
+) else (
+    REM Check for .env file in .\common
+    if exist ".\common\.env" (
+        set "env_file_path=.\common\.env"
+    ) else (
+        echo .env file not found in ..\common or .\common.
+        exit /b 1
+    )
+)
+REM Load the environment variables from the .env file
+echo Loading env variables from common .env file in local execution...
+for /F "tokens=1* delims==" %%a in (%env_file_path%) do (
+    set "TF_VAR_%%a=%%b"
+)
+
+REM Set variables that are blank
 set TF_VAR_MODEL_SYSTEM_MESSAGE=""
 
 echo Destroying Terraform resources!
